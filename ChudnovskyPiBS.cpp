146bcc39ff88801b3440b8dd4bec992258e44a41
Noman <baba.noman@live.com>
2022-11-26 16:20:06 +0100

    Bringing back old mult-threaded solution as an option.
    Turns out it was never wrong (I thought it was due to sleep deprivation). The only thing with the new multithreaded binary splitting function is that a barrier is used to make sure all worker threads are spawned before any of the others start taking CPU time (since worker threads have higher priority).

--- ChudnovskyPiBS.cpp
+++ ChudnovskyPiBS.cpp
@@ -8,7 +8,7 @@
 
 //Global variables:
 const int NUM_THREADS_IN_CPU = std::thread::hardware_concurrency();
-const int NUM_MAIN_WORKER_THREADS = 2*NUM_THREADS_IN_CPU;
+const int NUM_MAIN_WORKER_THREADS = NUM_THREADS_IN_CPU; // 2 * NUM_THREADS_IN_CPU;
 const int TOTAL_THREAD_COUNT = ChudnovskyPiBS::getTotalNumThreadsFromUsefulNumThreads(NUM_MAIN_WORKER_THREADS);
 std::barrier ALL_THREADS_SPAWNED(NUM_MAIN_WORKER_THREADS);
 
@@ -36,9 +36,6 @@ ChudnovskyPiBS::ChudnovskyPiBS(unsigned long _digits)
 	mpz_fdiv_q(intBigC3_OVER_24.get_mpz_t(), intBigC3_OVER_24.get_mpz_t(), twentyfour.get_mpz_t());
 	//intBigC3_OVER_24.get_str();
 }
-
-
-
 
 bsReturn ChudnovskyPiBS::bs(mpz_class a, mpz_class b) //clearly thread safe because nothing from outside the function is written to.
 {
@@ -92,10 +89,87 @@ bsReturn ChudnovskyPiBS::bs(mpz_class a, mpz_class b) //clearly thread safe beca
 	return result;
 }
 
+bsReturn ChudnovskyPiBS::bs_multithreaded(mpz_class a, mpz_class b, int threadCount) //clearly thread safe because nothing from outside the function is written to.
+{
+	bsReturn result;
+	mpz_class Pab;
+	mpz_class Qab;
+	mpz_class Tab;
+	mpz_class m;
+	if (b - a == 1)
+	{
+		//Directly compute P(a,a+1), Q(a,a+1) and T(a,a+1)
+		if (a == 0)
+			Pab = Qab = 1;
+		else
+		{
+			Pab = (6 * a - 5) * (2 * a - 1) * (6 * a - 1);
+			Qab = a * a * a * intBigC3_OVER_24;
+		}
+
+		Tab = Pab * (13591409 + 545140134 * a); // a(a) * p(a)
+		//Pab.get_str();
+		//Tab.get_str();
+		//a.get_str();
+		//Qab.get_str();
+		mpz_class toCheck;
+		mpz_and(toCheck.get_mpz_t(), a.get_mpz_t(), one);
+		if (toCheck == 1) //note to self: works as expected
+			Tab = -Tab;
+	}
+	else
+	{
+		// Recursively compute P(a,b), Q(a,b) and T(a,b)
+		// m is the midpoint of and b
+		mpz_class aplusb = a + b;
+		mpz_div_ui(m.get_mpz_t(), aplusb.get_mpz_t(), 2);//equivalent pseudocode: m=floor((a+b)/2)
+		// Recursively calculate P(a, m), Q(a, m) and T(a, m)
+		bsReturn am; am.Q = NULL;
+		std::future<bsReturn> futAm;
+		if (threadCount > 0)
+		{
+			futAm = std::async(std::launch::async, &ChudnovskyPiBS::bs_multithreaded, this, a, m, (--threadCount - 1) / 2);
+		}
+		else
+		{
+			SetThreadPriority(GetCurrentThread(), THREAD_PRIORITY_HIGHEST);
+			am = bs(a, m);
+		}
+		// Recursively calculate P(m, b), Q(m, b) and T(m, b)
+		bsReturn mb; mb.Q = NULL;
+		std::future<bsReturn> futMb;
+		if (threadCount > 0)
+		{
+			futMb = std::async(std::launch::async, &ChudnovskyPiBS::bs_multithreaded, this, m, b, (--threadCount) / 2);
+		}
+		else
+		{
+			SetThreadPriority(GetCurrentThread(), THREAD_PRIORITY_HIGHEST);
+			mb = bs(m, b);
+		}
+
+		//Wait for threads:
+		threadCount -= 2;
+		if (am.Q == NULL)
+			am = futAm.get();
+		if (mb.Q == NULL)
+			mb = futMb.get();
+		// Now combine
+		Pab = am.P * mb.P;
+		Qab = am.Q * mb.Q;
+		Tab = (mb.Q * am.T) + (am.P * mb.T);
+	}
+	result.P = Pab;
+	result.Q = Qab;
+	result.T = Tab;
+	//std::string pabStr = result.P.get_str();
+	//std::string qabStr = result.Q.get_str();
+	//std::string tabStr = result.T.get_str();
+	return result;
+}
 
 
-
-bsReturn ChudnovskyPiBS::bs_multithreaded(mpz_class a, mpz_class b, int threadCount, int depth) //clearly thread safe because nothing from outside the function is written to.
+bsReturn ChudnovskyPiBS::bs_multithreaded_barrier(mpz_class a, mpz_class b, int threadCount, int depth) //clearly thread safe because nothing from outside the function is written to.
 {
 	if (threadCount == 0)
 	{
@@ -141,7 +215,7 @@ bsReturn ChudnovskyPiBS::bs_multithreaded(mpz_class a, mpz_class b, int threadCo
 		if (threadCount > 0)
 		{
 			int subThreadCountLeft = (--threadCount - 1) / 2; //(int)powl(2, depth); //About the following comment, I might have been wrong. Can't blame me, I'm sleep deprived. //you divide threadCount by powl(2,depth) because 2^depth tells you the number of parallel threads of the binary recursion (or the nodes of a binary tree) at this depth and so you can calculate how to divide number of threads to spawn.
-			futAm = std::async(std::launch::async, &ChudnovskyPiBS::bs_multithreaded, this, a, m, subThreadCountLeft, depth); 
+			futAm = std::async(std::launch::async, &ChudnovskyPiBS::bs_multithreaded_barrier, this, a, m, subThreadCountLeft, depth); 
 		}
 		else
 		{
@@ -156,7 +230,7 @@ bsReturn ChudnovskyPiBS::bs_multithreaded(mpz_class a, mpz_class b, int threadCo
 		if (threadCount > 0)
 		{
 			int subThreadCountRight = (--threadCount) / 2;//(int)powl(2, depth); //About the following comment, I might have been wrong. Can't blame me, I'm sleep deprived. //you divide threadCount by powl(2,depth) because 2^depth tells you the number of parallel threads of the binary recursion (or the nodes of a binary tree) at this depth and so you can calculate how to divide number of threads to spawn.
-			futMb = std::async(std::launch::async, &ChudnovskyPiBS::bs_multithreaded, this, m, b, subThreadCountRight, depth);
+			futMb = std::async(std::launch::async, &ChudnovskyPiBS::bs_multithreaded_barrier, this, m, b, subThreadCountRight, depth);
 		}
 		else
 		{
@@ -211,22 +285,14 @@ mpz_class ChudnovskyPiBS::calculatePi()
 {
 	SetPriorityClass(GetCurrentProcess(), ABOVE_NORMAL_PRIORITY_CLASS);
 
-
-	//for (int i = 1; i < 5; i++)
-	//{
-	//	std::cout << (int)pow(2, i) <<" - " << getTotalNumThreadsFromUsefulNumThreads((int)pow(2,i)) << std::endl;
-	//}
-
 	//std::cout << NUM_THREADS_IN_CPU << std::endl;
 	//std::cout << TOTAL_THREAD_COUNT << std::endl;
-	bsReturn BSResult = bs_multithreaded(0, N, TOTAL_THREAD_COUNT, 0);//bs(0, N); 
-	//bsReturn BSResult = bs(0, N); //apparently Q and T gotten are wrong.
+	bsReturn BSResult = bs_multithreaded_barrier(0, N, TOTAL_THREAD_COUNT, 0); //bs_multithreaded(0, N, TOTAL_THREAD_COUNT); //bs_multithreaded_barrier(0, N, TOTAL_THREAD_COUNT, 0); //bs(0, N); //apparently Q and T gotten are wrong.
 	//BSResult.Q.get_str();
 	//BSResult.T.get_str();
 	//return mpz_fdiv_q((Q * 426880 * sqrtC) / T
 	mpz_class result = (BSResult.Q * 426880 * sqrtC);
 	mpz_fdiv_q(result.get_mpz_t(), result.get_mpz_t(), BSResult.T.get_mpz_t());
-
 
 	SetPriorityClass(GetCurrentProcess(), NORMAL_PRIORITY_CLASS);
 	return result;
@@ -236,85 +302,7 @@ mpz_class ChudnovskyPiBS::calculatePi()
 /* DEPRECATED/OLD MULTITHREADING SOLUTION
 * //The following definition made 16 main-worker-threads instead of 8 when asked to make 8.
 
-bsReturn ChudnovskyPiBS::bs_multithreaded(mpz_class a, mpz_class b, int threadCount, int depth) //clearly thread safe because nothing from outside the function is written to.
-{
-	depth++;
-	bsReturn result;
-	mpz_class Pab;
-	mpz_class Qab;
-	mpz_class Tab;
-	mpz_class m;
-	if (b - a == 1)
-	{
-		//Directly compute P(a,a+1), Q(a,a+1) and T(a,a+1)
-		if (a == 0)
-			Pab = Qab = 1;
-		else
-		{
-			Pab = (6 * a - 5) * (2 * a - 1) * (6 * a - 1);
-			Qab = a * a * a * intBigC3_OVER_24;
-		}
 
-		Tab = Pab * (13591409 + 545140134 * a); // a(a) * p(a)
-		//Pab.get_str();
-		//Tab.get_str();
-		//a.get_str();
-		//Qab.get_str();
-		mpz_class toCheck;
-		mpz_and(toCheck.get_mpz_t(), a.get_mpz_t(), one);
-		if (toCheck == 1) //note to self: works as expected
-			Tab = -Tab;
-	}
-	else
-	{
-		// Recursively compute P(a,b), Q(a,b) and T(a,b)
-		// m is the midpoint of and b
-		mpz_class aplusb = a + b;
-		mpz_div_ui(m.get_mpz_t(), aplusb.get_mpz_t(), 2);//equivalent pseudocode: m=floor((a+b)/2)
-		// Recursively calculate P(a, m), Q(a, m) and T(a, m)
-		bsReturn am; am.Q = NULL;
-		std::future<bsReturn> futAm;
-		if (threadCount > 0)
-		{
-			futAm = std::async(std::launch::async, &ChudnovskyPiBS::bs_multithreaded, this, a, m, (--threadCount - 1) / 2, depth);
-		}
-		else
-		{
-			SetThreadPriority(GetCurrentThread(), THREAD_PRIORITY_HIGHEST);
-			am = bs(a, m);
-		}
-		// Recursively calculate P(m, b), Q(m, b) and T(m, b)
-		bsReturn mb; mb.Q = NULL;
-		std::future<bsReturn> futMb;
-		if (threadCount > 0)
-		{
-			futMb = std::async(std::launch::async, &ChudnovskyPiBS::bs_multithreaded, this, m, b, (--threadCount) / 2, depth);
-		}
-		else
-		{
-			SetThreadPriority(GetCurrentThread(), THREAD_PRIORITY_HIGHEST);
-			mb = bs(m, b);
-		}
-
-		//Wait for threads:
-		threadCount -= 2;
-		if (am.Q == NULL)
-			am = futAm.get();
-		if (mb.Q == NULL)
-			mb = futMb.get();
-		// Now combine
-		Pab = am.P * mb.P;
-		Qab = am.Q * mb.Q;
-		Tab = (mb.Q * am.T) + (am.P * mb.T);
-	}
-	result.P = Pab;
-	result.Q = Qab;
-	result.T = Tab;
-	//std::string pabStr = result.P.get_str();
-	//std::string qabStr = result.Q.get_str();
-	//std::string tabStr = result.T.get_str();
-	return result;
-}
 
 
 mpz_class ChudnovskyPiBS::calculatePi()
--- ChudnovskyPiBS.h
+++ ChudnovskyPiBS.h
@@ -44,7 +44,8 @@ private:
 	/// <param name="b"></param>
 	/// <returns></returns>
 	bsReturn bs(mpz_class a, mpz_class b);
-	bsReturn bs_multithreaded(mpz_class a, mpz_class b, int threadCount, int depth);
+	bsReturn bs_multithreaded(mpz_class a, mpz_class b, int threadCount);
+	bsReturn bs_multithreaded_barrier(mpz_class a, mpz_class b, int threadCount, int depth); //uses a barrier to wait for all main worker threads to spawn.
 
 public:
 	/// <summary>
