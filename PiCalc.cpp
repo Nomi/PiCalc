258bec1963c65d3e1d727a870f471f9787d22e10
Noman <baba.noman@live.com>
2022-12-04 22:23:35 +0100

    Added part for compatibility prog

--- PiCalc.cpp
+++ PiCalc.cpp
@@ -4,9 +4,11 @@
 #include <chrono>
 #include "GMP/gmpxx.h"
 #include "ChudnovskyPiBS.h"
-
+#include <string>
+#include "Windows.h"
 #define OUTPUT_TXT_FILEPATH "./calculated_pi.txt"
-
+#define WMPIR_COMPATIBILITY_PROGRAM_PATH L"./pcwmpir-CompatabilityProgram.exe"
+#define HUNDRED_MILLION 100000000
 int main()
 {
     ///Configuration
@@ -15,7 +17,47 @@ int main()
     std::cout << "Enter the number of digits to calculate (excluding initial 3): ";
     std::cin >> digits;
     std::cout << std::endl;
+    if (digits > HUNDRED_MILLION)
+    {
+        STARTUPINFO si;
+        PROCESS_INFORMATION pi;
 
+        ZeroMemory(&si, sizeof(si));
+        si.cb = sizeof(si);
+        ZeroMemory(&pi, sizeof(pi));
+
+
+        std::wstring wpath=WMPIR_COMPATIBILITY_PROGRAM_PATH;
+        std::wstring wdigits = std::to_wstring(digits);
+        std::wstring wcmnd = wpath + L" " + wdigits;
+        LPWSTR cmnd = LPWSTR(wcmnd.c_str());
+        // Start the child process. 
+        if (!CreateProcess(NULL,   // No module name (use command line)
+            cmnd,        // Command line
+            NULL,           // Process handle not inheritable
+            NULL,           // Thread handle not inheritable
+            FALSE,          // Set handle inheritance to FALSE
+            0,              // No creation flags
+            NULL,           // Use parent's environment block
+            NULL,           // Use parent's starting directory 
+            &si,            // Pointer to STARTUPINFO structure
+            &pi)           // Pointer to PROCESS_INFORMATION structure
+            )
+        {
+            printf("CreateProcess failed for wmpir Compatility Program (%d).\n", GetLastError());
+            return -96;
+        }
+
+        // Wait until child process exits.
+        WaitForSingleObject(pi.hProcess, INFINITE);
+
+        // Close process and thread handles. 
+        CloseHandle(pi.hProcess);
+        CloseHandle(pi.hThread);
+        
+        //return from this prog
+        return 96;
+    }
     ///Computing Pi
     std::cout << "Starting computation." << std::endl;
     auto start = std::chrono::high_resolution_clock::now();
